ed60a50ad92796452f2a234a2d3bd968640f1611
문지욱 <jiukmoon@munjiug-ui-MacBookAir.local>
2024-07-19 23:53:52 +0900

    First Test Deployment

--- src/Components/HikingMap.js
+++ src/Components/HikingMap.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react';
+import MarkerInfo from '../Image/MarkerInfo.png';
+import MarkerBlack from '../Image/MarkerBlack.png';
+import MarkerBlue from '../Image/MarkerBlue.png';
+import MarkerOrange from '../Image/MarkerOrange.png';
+import MarkerRed from '../Image/MarkerRed.png';
+import HikingRiskAreas from '../Data/hikingRiskAreas.json';
+import MountainPeaks from '../Data/mountainPeaks.json';
 
 /* global kakao */
 
 const HikingMap = () => {
+    const [map, setMap] = useState(null); // 지도 객체를 저장하는 상태 변수
+    const mapContainer = useRef(null); // 지도 컨테이너를 참조하는 useRef 객체
+    const mountainAreas = HikingRiskAreas // 산악 위험 지역 정보를 저장하는 상태 변수
+    const mountainPeaks = MountainPeaks // 산악 봉우리 정보를 저장하는 상태 변수
     const [inputText, setInputText] = useState(''); // 입력 상태를 관리하는 상태 변수
     const [places, setPlaces] = useState([]); // 장소 검색 결과를 저장하는 상태 변수
-    const [mountainAreas, setMountainAreas] = useState([]); // 산악 위험 지역 정보를 저장하는 상태 변수
-    const [mountainPeaks, setMountainPeaks] = useState([]); // 산악 봉우리 정보를 저장하는 상태 변수
-    const [tourismSpots, setTourismSpots] = useState([]); // 주변 관광지 정보를 저장하는 상태 변수
     const [selectedLocation, setSelectedLocation] = useState(null); // 선택된 위치 정보를 저장하는 상태 변수
-    const mapContainer = useRef(null); // 지도 컨테이너를 참조하는 useRef 객체
-    const [map, setMap] = useState(null); // 지도 객체를 저장하는 상태 변수
+    const [tourismSpots, setTourismSpots] = useState([]); // 주변 관광지 정보를 저장하는 상태 변수
+    
+
+
+
 
     // Kakao Maps API 스크립트 로드 후, 지도 초기화
     useEffect(() => {
         const script = document.createElement('script');
         script.async = true;
-        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=a164be8492b119c1495b8ef43618cd64&autoload=false&libraries=services,clusterer`;
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=7addc1cfbf3f649d565d0467de87b27c&autoload=false&libraries=services,clusterer`;
         document.head.appendChild(script);
 
         script.onload = () => {
@@ -29,30 +40,57 @@ const HikingMap = () => {
                 const newMap = new kakao.maps.Map(container, options); // 새로운 지도 객체 생성
                 setMap(newMap); // 생성된 지도 객체를 상태 변수에 저장
             });
-
-            
         };
     }, []);
 
-    
-    // 산악 위험 지역 및 봉우리 데이터를 가져오는 효과
+    // 이펙트 함수는 지도 및 산악 정보(mountainAreas 및 mountainPeaks)가 변경될 때 호출됩니다.
     useEffect(() => {
-        const fetchMountainData = async () => {
-            const response = await fetch('/Data/hikingRiskAreas.json');
-            const data = await response.json();
-            setMountainAreas(data); // 산악 위험 지역 정보 설정
-        };
-        const fetchMountainPeaks = async () => {
-            const response = await fetch('/Data/mountainPeaks.json');
-            const data = await response.json();
-            setMountainPeaks(data); // 산악 봉우리 정보 설정
-        };
-        fetchMountainData();
-        fetchMountainPeaks();
-    }, []);
+        // 지도와 산악 지역 및 봉우리 데이터가 준비되어 있을 때만 실행합니다.
+        if (map) {
+            // 각 산봉우리에 대한 마커를 생성하고 설정합니다.
+            mountainPeaks.forEach(peak => {
+                const marker = new kakao.maps.Marker({
+                    map: map,
+                    position: new kakao.maps.LatLng(peak.lat, peak.lot),
+                    title: peak.placeNm
+                });
+                // 봉우리 마커 이미지를 설정합니다.
+                marker.setImage(new kakao.maps.MarkerImage(
+                    MarkerBlack, 
+                    new kakao.maps.Size(24, 35)
+                ));
+            });
 
-    // 입력 텍스트 변경 시 호출되는 핸들러 함수
-    const handleInputChange = (e) => {
+            // 각 산악 지역에 대한 마커를 생성하고 설정합니다.
+            mountainAreas.forEach(area => {
+                let imageSrc;
+                // 산악 지역 유형에 따라 마커 이미지를 선택합니다.
+                switch (area.plcTypeCd) {
+                    case 'DANGER':
+                        imageSrc = MarkerRed;
+                        break;
+                    case 'SHELTER':
+                        imageSrc = MarkerOrange;
+                        break;
+                    default:
+                        imageSrc = MarkerRed;
+                }
+                const marker = new kakao.maps.Marker({
+                    map: map,
+                    position: new kakao.maps.LatLng(area.lat, area.lot),
+                    title: area.plcNm
+                });
+                // 산악 지역 마커 이미지를 설정합니다.
+                marker.setImage(new kakao.maps.MarkerImage(
+                    imageSrc, 
+                    new kakao.maps.Size(24, 35)
+                ));
+            });
+        }
+    }, [map]);
+
+     // 입력 텍스트 변경 시 호출되는 핸들러 함수
+     const handleInputChange = (e) => {
         const { value } = e.target;
         setInputText(value);
         if (!value.trim()) {
@@ -76,9 +114,9 @@ const HikingMap = () => {
 
     // 장소 선택 시 호출되는 핸들러 함수
     const handlePlaceSelect = (place) => {
-        console.log(`Selected place: ${place.place_name}`);
-        console.log(`Address: ${place.address_name}`);
-        console.log(`Coordinates: Latitude ${place.y}, Longitude ${place.x}`);
+        //console.log(`Selected place: ${place.place_name}`);
+        //console.log(`Address: ${place.address_name}`);
+        //console.log(`Coordinates: Latitude ${place.y}, Longitude ${place.x}`);
 
         // 선택된 위치 좌표 설정
         setSelectedLocation({
@@ -98,7 +136,7 @@ const HikingMap = () => {
                 title: place.place_name
             });
             marker.setImage(new kakao.maps.MarkerImage(
-                '/Image/MarkerBlue.png', 
+                MarkerBlue, 
                 new kakao.maps.Size(24, 35)
             ));
         }
@@ -108,13 +146,13 @@ const HikingMap = () => {
     useEffect(() => {
         const fetchNearbyTourismSpots = async () => {
             if (selectedLocation) {
-                const url = 'http://apis.data.go.kr/B551011/KorService1/locationBasedList1';
+                const url = 'https://apis.data.go.kr/B551011/KorService1/locationBasedList1';
                 const params = new URLSearchParams({
-                    serviceKey: '5PRZEFOO-5PRZ-5PRZ-5PRZ-5PRZEFOO6H',
+                    serviceKey: 'U62bzn4ae8kub7VNW4NViYCyj8hZUzULvAtCJyigub1NG69v+ex6yXtooFqjF9n4BS+xLX5HX/uy29vTA5Iv/A==',
                     numOfRows: '20',
                     pageNo: '1',
                     MobileOS: 'ETC',
-                    MobileApp: 'TravelVigil',
+                    MobileApp: 'SafeHikingTour',
                     arrange: 'A',
                     mapX: selectedLocation.longitude.toString(),
                     mapY: selectedLocation.latitude.toString(),
@@ -136,52 +174,16 @@ const HikingMap = () => {
         fetchNearbyTourismSpots();
     }, [selectedLocation]);
 
-    // 지도 및 산악 관련 정보 변경 시 호출되는 효과
-    useEffect(() => {
-        if (map && mountainAreas.length && mountainPeaks.length) {
-            mountainPeaks.forEach(peak => {
-                const marker = new kakao.maps.Marker({
-                    map: map,
-                    position: new kakao.maps.LatLng(peak.lat, peak.lot),
-                    title: peak.placeNm
-                });
-                marker.setImage(new kakao.maps.MarkerImage(
-                    '/Image/MarkerBlack.png', 
-                    new kakao.maps.Size(24, 35)
-                ));
-            });
 
-            mountainAreas.forEach(area => {
-                let imageSrc;
-                switch (area.plcTypeCd) {
-                    case 'DANGER':
-                        imageSrc = '/Image/MarkerRed.png';
-                        break;
-                    case 'SHELTER':
-                        imageSrc = '/Image/MarkerOrange.png';
-                        break;
-                    default:
-                        imageSrc = '/Image/MarkerRed.png';
-                }
-                const marker = new kakao.maps.Marker({
-                    map: map,
-                    position: new kakao.maps.LatLng(area.lat, area.lot),
-                    title: area.plcNm
-                });
-                marker.setImage(new kakao.maps.MarkerImage(
-                    imageSrc, 
-                    new kakao.maps.Size(24, 35)
-                ));
-            });
-        }
-    }, [map, mountainAreas, mountainPeaks]);
 
+
+    
     return (
         <div className='hikingMap'>
             <div className='mapContainer' ref={mapContainer}/>
             
             <div className='info'>
-                <img className='markerInfoImg' src="/Image/MarkerInfo.png" alt="Example" />
+                <img className='markerInfoImg' src={MarkerInfo} alt="마커 설명" />
                 <div className='search'>
                     <input
                         type="text"
